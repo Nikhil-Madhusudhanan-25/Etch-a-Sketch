b47a35529919218282a8f11d10cba00594d8d65d
nikhil-madhusudhanan-25 <nikhilmadhusudhanan20@gmail.com>
2023-11-10 15:25:56 +0530

    Create 256 divs on the page

---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+let containerDiv= document.getElementById('container');
+for(let i=0;i<15;i++)
+ for(let j=0;j<15;j++)
+    {   let squareDiv= document.createElement('div');
+        containerDiv.appendChild(squareDiv);}
